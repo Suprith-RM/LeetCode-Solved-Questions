17f3012ee01aa80a83be0f3eb321766abdb155cd
Suprith-RM <suprithrm07@gmail.com>
2026-07-02 22:43:27 +0530

    Time: 26 ms (6.37%), Space: 59.9 MB (5.71%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int carry = 0, i = num1.size()-1, j = num2.size()-1;
+        string ans = "";
+        while(carry || i >= 0 || j >= 0 ){
+            int sum = 0;
+            if(i >= 0){
+                sum += num1[i] - '0';
+                i--;
+            } 
+            if(j >= 0){
+                sum += num2[j] - '0';
+                j--;
+            }
+            sum += carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            ans = to_string(sum) + ans;
+        }
+        return ans;
+    }
+};
