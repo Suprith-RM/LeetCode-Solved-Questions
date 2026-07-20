cbdef540ed6f573c3a6502dedac3845b16e06e43
Suprith-RM <suprithrm07@gmail.com>
2026-07-20 13:29:53 +0530

    Time: 0 ms (100%), Space: 18.6 MB (22.37%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        int n = grid.size();
-        int m = grid[0].size();
-        for(int i = 0; i < k; ++i){
-            int temp = grid[n-1][m-1];
-            for(int j = n-1; j > 0; --j){
-                grid[j][m-1] = grid[j-1][m-1];
-            }
-            grid[0][m-1] = temp;
-            for(int j = 0; j < n; ++j){
-                int temp = grid[j][m-1];
-                for(int k = m-1; k > 0 ; --k){
-                    grid[j][k] = grid[j][k-1];
-                }
-                grid[j][0] = temp;
-            }
-        }
+        int n = grid.size(), m = grid[0].size();
+        vector<int> shift(m * n);
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+                shift[(i * m) + j] = grid[i][j];
+        int rotate_times = k % (m * n);
+        reverse(shift.begin(), shift.end());
+        reverse(shift.begin(), shift.begin() + rotate_times);
+        reverse(shift.begin() + rotate_times, shift.end());
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+                grid[i][j] = shift[(i * m) + j];   
         return grid;
     }
 };
