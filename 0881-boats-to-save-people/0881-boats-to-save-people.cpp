cb1d10948b725a239634eb46ec85ee209f8a31df
Suprith-RM <suprithrm07@gmail.com>
2026-08-10 18:06:52 +0530

    Time: 18 ms (52.91%), Space: 45.6 MB (97.33%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -4,23 +4,12 @@ public:
         sort(people.begin(), people.end());
         int n = people.size(), i = 0, j = n-1, cnt = 0;
         while(i <= j){
-            if(i != j && people[i] + people[j] <= limit){
-                cnt++;
-                people[i] = 0;
-                people[j] = 0;
-                i++; 
-                j--;
-                continue;
+            if(people[i] + people[j] <= limit){
+                i++;
             }
-            else j--;
+            j--;
+            cnt++;
         }
-        sort(people.begin(), people.end());
-        i = 0;
-        for(i = cnt * 2; i < n; i+=2){
-            if(i < n-1 && people[i] + people[i+1] <= limit) cnt++;
-            else break;
-        }
-        cnt += (n-i);
         return cnt;
     }
 };
